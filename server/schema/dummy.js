e2ff0baba8634604ec6a6e9f5ee054f9d9919c3b
Deborah Oni <deborah.oni@andela.com>
2020-01-27 20:46:51 +0100

    schema built + basic queries

---
+++ server/schema/dummy.js
@@ -0,0 +1,18 @@
+const dummy = {
+books : [
+  {name: 'War of art', genre:'Fantasy',id:'1', authorId:'1'},
+  {name: 'Art', genre:'Sci-Fi',id:'2', authorId:'1'},
+  {name: 'Science', genre:'Thriller',id:'3', authorId:'2'},
+  {name: 'Economics', genre:'Comedy',id:'4', authorId:'2'},
+  {name: 'Wars', genre:'Fantasy',id:'5', authorId:'3'}
+  ],
+
+authors : [
+  {name:'Levi Levington', age: 54, id:'1'},
+  {name:'Stevie Levington', age: 54, id:'2'},
+  {name:'John Levington', age: 54, id:'3'},
+]
+}
+
+
+module.exports = dummy
